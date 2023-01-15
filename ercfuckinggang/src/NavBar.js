c926b61f63b2ca70912a43615313d7e1f924730f
Jay Shin <shin.810@osu.edu>
2023-01-15 11:53:33 -0500

    ?

--- ercfuckinggang/src/NavBar.js
+++ ercfuckinggang/src/NavBar.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import {Link, Route, Routes, BrowserRouter } from 'react-router-dom';
 
 export const NavBar = () => {
     
     return (
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="/"><img className="main-logo" src="./sources/ercf.png"></img></a>
+        <Link class="navbar-brand" to="/ERCF"><img className="main-logo" src="./sources/ercf.png"></img></Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="/ERCF/About">About</a>
+              <Link class="nav-link" to="/About">About</Link>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="/ERCF/Artists">Artists</a>
+              <Link class="nav-link" to="/Artists">Artists</Link>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="/ERCF/Music">Music</a>
+              <Link class="nav-link" to="/Music">Music</Link>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="/ERCF/Contact">Contact</a>
+              <Link class="nav-link" to="/Contact">Contact</Link>
             </li>
           </ul>
         </div>
